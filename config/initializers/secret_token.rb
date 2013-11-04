121d46e9f7b627137fee46488f34ce81fba7c53a
phppirate <sam@podlogar.com>
2013-11-04 15:05:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '07a48bbaf2929c062a3b8807ff119a927f8d1e78fa0a97fc8dae341e6bfb9a3f03996ef2def7fee02a23874ddbe89dfa8524f21dd8fadde0d70ed5c33690953d'

9bebb6ea769649b03800356fa365e6548b9475d5
phppirate <sam@podlogar.com>
2013-11-11 08:18:39 -0600

    Fixed Small session_helper.rb Error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = { value: user.remember_token }   
-    current_user = user
+    self.current_user = user
   end
 
   def signed_in?
@@ -11,7 +11,7 @@ module SessionsHelper
 
   def sign_out
     cookies.delete(:remember_token)
-    current_user = nil
+    self.current_user = nil
   end
 
   def current_user=(user)
